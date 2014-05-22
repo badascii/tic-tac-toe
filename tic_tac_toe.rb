dccdf38fa5ae61dbcf844407cd55a7dda30e7feb
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-05-21 23:07:44 -0700

    built out grid hash structure in new version

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,10 +1,26 @@
 class Game
+
+  GRID = {'a1': 0, 'a2': 0, 'a3': 0,
+          'b1': 0, 'b2': 0, 'b3': 0,
+          'c1': 0, 'c2': 0, 'c3': 0}
+
   def initialize
     @player = Player.new
     @cpu = Cpu.new
     @player_mark =
     @cpu_mark    = "0"
   end
+
+  def run
+    until game_over?
+
+    end
+  end
+
+  def game_over?
+
+  end
+
 end
 
 class Player
@@ -18,3 +34,7 @@ class Cpu
     @mark = "O"
   end
 end
+
+game = Game.new
+
+game.run
