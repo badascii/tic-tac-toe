8f92c1ee55cd36b41897b51481747fd0cf1b79a0
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-06-24 19:53:34 -0700

    added unit test for three_in_a_row method and fixed opposite corner tests

--- test/test.rb
+++ test/test.rb
@@ -51,6 +51,14 @@ class TestGame < MiniTest::Unit::TestCase
     assert(@game.win?(@game.player_mark))
   end
 
+  def test_three_in_a_row
+    assert(!@game.three_in_a_row?(@player_mark, ["a1", "a2", "a3"]))
+    @game.grid["a1"] = @player_mark
+    @game.grid["a2"] = @player_mark
+    @game.grid["a3"] = @player_mark
+    assert(@game.three_in_a_row?(@player_mark, ["a1", "a2", "a3"]))
+  end
+
   def test_cpu_opening_move
     assert_equal(@game.grid["b2"], 0)
     @game.opening_move
@@ -97,15 +105,15 @@ class TestGame < MiniTest::Unit::TestCase
 
   def test_cpu_opposite_corners_1
     assert(!@game.opposite_corners?)
-    @game.grid["a1"] == @game.player_mark
-    @game.grid["c3"] == @game.player_mark
+    @game.grid["a1"] = @game.player_mark
+    @game.grid["c3"] = @game.player_mark
     assert(@game.opposite_corners?)
   end
 
   def test_cpu_opposite_corners_2
     assert(!@game.opposite_corners?)
-    @game.grid["a3"] == @game.player_mark
-    @game.grid["c1"] == @game.player_mark
+    @game.grid["a3"] = @game.player_mark
+    @game.grid["c1"] = @game.player_mark
     assert(@game.opposite_corners?)
   end
 end
